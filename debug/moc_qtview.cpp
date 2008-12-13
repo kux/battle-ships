0178c4cc08f019e83cd3b2a31099d8f6cf25d4b0
kux <kux@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2008-12-13 04:53:10 +0000

    release version :P

--- debug/moc_qtview.cpp
+++ debug/moc_qtview.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'qtview.h'
 **
-** Created: Mon Dec 1 15:22:35 2008
+** Created: Sat Dec 13 06:52:30 2008
 **      by: The Qt Meta Object Compiler version 59 (Qt 4.4.3)
 **
 ** WARNING! All changes made in this file will be lost!
--- ui_qtview.h
+++ ui_qtview.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'qtview.ui'
 **
-** Created: Mon Dec 1 15:22:21 2008
+** Created: Sat Dec 13 06:52:15 2008
 **      by: Qt User Interface Compiler version 4.4.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
